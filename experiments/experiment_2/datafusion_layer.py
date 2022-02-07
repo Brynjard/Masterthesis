737d0695059c92d0f41309550ac5fb94ee9d9319
Håkon Hernes <haakhern@g001.cm.cluster>
2022-02-07 10:39:53 +0100

    Script for counting preds

--- experiments/experiment_2/datafusion_layer.py
+++ experiments/experiment_2/datafusion_layer.py
@@ -4,11 +4,12 @@ import sys
 PARENT_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 sys.path.append(PARENT_FOLDER)
 import modify_labels_utils as utils
+import datafusion_utils as fusion_utils
 
 """
 This model combines the output of three models(prev, curr, next) in the dict-representation established above, and outputs a single truth for predictions. 
 """
-def ex2_data_fusion(prev_model, curr_model, next_model, confidence_threshold, time_adjustment):
+def ex2_data_fusion(prev_model, curr_model, next_model, time_adjustment, time_frame):
     predictions_dictionary = {}
     for game_url in curr_model.keys():
         url = curr_model[game_url]["url"]
@@ -17,20 +18,23 @@ def ex2_data_fusion(prev_model, curr_model, next_model, confidence_threshold, ti
         prev_predictions = prev_model[game_url]["predictions"]
         next_predictions = next_model[game_url]["predictions"]
 
-        #filtering on confidence: 
-        current_predictions = [p for p in current_predictions if float(p["confidence"]) >= confidence_threshold]
-        prev_predictions = [p for p in prev_predictions if float(p["confidence"]) >= confidence_threshold]
-        next_predictions = [p for p in next_predictions if float(p["confidence"]) >= confidence_threshold]
-    
+        for p in prev_predictions:
+            p["model"] = "past"
+        
+        for p in next_predictions:
+            p["model"] = "future"
+        
+        for p in current_predictions:
+            p["model"] = "current"
 
         #Adjusting time for prev and next models' predictions:
-        prev_predictions = [utils.add_or_subtract_time(p, 0 - time_adjustment) for p in prev_predictions]
-        next_predictions = [utils.add_or_subtract_time(p, time_adjustment) for p in next_predictions]
+        prev_predictions = [fusion_utils.add_or_subtract_time(p, 0 - time_adjustment) for p in prev_predictions]
+        next_predictions = [fusion_utils.add_or_subtract_time(p, time_adjustment) for p in next_predictions]
         
         # Merge the predictions into 1 list
-        new_prev_predictions = [p for p in prev_predictions if not utils.predicted_event_exists(current_predictions=current_predictions, prediction_object_to_check=p, time_frame=10000)]
+        new_prev_predictions = [p for p in prev_predictions if not fusion_utils.predicted_event_exists(current_predictions=current_predictions, prediction_object_to_check=p, time_frame=time_frame)]
         all_predictions = current_predictions + new_prev_predictions
-        new_next_predictions = [p for p in next_predictions if not utils.predicted_event_exists(current_predictions=all_predictions, prediction_object_to_check=p, time_frame=10000)]
+        new_next_predictions = [p for p in next_predictions if not fusion_utils.predicted_event_exists(current_predictions=all_predictions, prediction_object_to_check=p, time_frame=time_frame)]
         all_predictions = all_predictions + new_next_predictions
         
         # Add game to the prediction dict    
@@ -42,22 +46,22 @@ def ex2_data_fusion(prev_model, curr_model, next_model, confidence_threshold, ti
     
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) < 7:
-        print(f" 6 command line arguments expected, {len(args) - 1} found")
+    if len(args) < 6:
+        print(f" 5 command line arguments expected, {len(args) - 1} found")
         exit()
     #Have to take these arguments: Source_prev source_curr source_next output_folder:
     source_prev = args[1]
     source_current = args[2]
     source_next = args[3]
     dest = args[4]
-    confidence_threshold = float(args[5])
-    time_adjustment = int(args[6])
+    time_adjustment = 25000
+    time_frame = int(args[5]) * 1000
 
-    utils.create_file_structure(source_prev, dest) #all models (prev, current, next) has same filestructure, so we might aswell use the prev-folder.
-    prev_model = utils.create_prediction_dict(source_prev)
-    current_model = utils.create_prediction_dict(source_current)
-    next_model = utils.create_prediction_dict(source_next)
+    fusion_utils.create_file_structure(source_prev, dest) #all models (prev, current, next) has same filestructure, so we might aswell use the prev-folder.
+    prev_model = fusion_utils.create_prediction_dict(source_prev)
+    current_model = fusion_utils.create_prediction_dict(source_current)
+    next_model = fusion_utils.create_prediction_dict(source_next)
 
-    prediction_dict = ex2_data_fusion(prev_model, current_model, next_model, confidence_threshold, time_adjustment)
-    utils.write_predictions(dest_folder=dest, prediction_dict=prediction_dict)
+    prediction_dict = ex2_data_fusion(prev_model, current_model, next_model, time_adjustment, time_frame)
+    fusion_utils.write_predictions(dest_folder=dest, prediction_dict=prediction_dict)
 
