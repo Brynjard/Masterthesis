90bc2e02263a96deee0d5909fc6e7efa5f98cb2a
Håkon Hernes <haakhern@g001.cm.cluster>
2022-03-07 13:07:19 +0100

    Functions for calculating TP, FP and FN

--- experiments/alternative_evaluation.py
+++ experiments/alternative_evaluation.py
@@ -1,4 +1,6 @@
+from cProfile import label
 import os
+from turtle import pos
 import numpy as np
 
 from SoccerNet.utils import getListGames
@@ -9,20 +11,56 @@ import glob
 import json
 import zipfile
 from tqdm import tqdm
+from collections import defaultdict
 
-def evaluate_no_delta(SoccerNet_path, Predictions_path, prediction_file="results_spotting.json", split="test", version=2, framerate=None, metric="loose"):
-    framerate = 2
+def evaluate_no_delta(SoccerNet_path, Predictions_path, prediction_file="results_spotting.json", split="test", version=2):
     list_games = getListGames(split=split)
-    targets_numpy = list()
-    detections_numpy = list()
-    closests_numpy = list()
+    ## dict with TP, FP and FN per class
 
-    
-    ## dict wit
-    
-
+    ## classes
+    """
+    [
+        "goal": {
+            "True positive": 3,
+            "False positive": 3,
+            "False negative": 5,
+        },
+        "corner": {
+            "True positive": 4,
+            "False positive": 7,
+            "False negative": 8,
+        },
+    ]
+    """ 
+    events = ['Penalty',
+                'Kick-off',
+                'Goal',
+                'Substitution',
+                'Offside',
+                'Shots on target',
+                'Shots off target',
+                'Clearance',
+                'Ball out of play',
+                'Throw-in',
+                'Foul',
+                'Indirect free-kick',
+                'Direct free-kick',
+                'Corner',
+                'Yellow card',
+                'Red card',
+                'Yellow->red card']
+    # Setup dict
+    metric_dict = dict()
+    for event_name in EVENT_DICTIONARY_V2.keys():
+        metric_dict[event_name] = defaultdict(lambda: 0)
+        # {"True positive": 0, "False positive": 0, "False negative": 0}
 
     for game in tqdm(list_games):
+        
+        # dict with predicion per class
+        prediction_dict = defaultdict(lambda: 0)
+        # dict with labels per class
+        labels_dict = defaultdict(lambda: 0)
 
         # Load labels
         if version==2:
@@ -38,73 +76,82 @@ def evaluate_no_delta(SoccerNet_path, Predictions_path, prediction_file="results
         # convert labels to vector
         label_half_1, label_half_2 = label2vector(
             labels, num_classes=num_classes, version=version)
-        print(label_half_1.shape)
-        # print(version)
-        # print(label_half_2)
 
 
+        # print("----------------------------------------")
+        # print(labels["UrlLocal"])
 
-        # infer name of the prediction_file
-        if prediction_file == None:
-            if zipfile.is_zipfile(Predictions_path):
-                with zipfile.ZipFile(Predictions_path, "r") as z:
-                    for filename in z.namelist():
-                        #       print(filename)
-                        if filename.endswith(".json"):
-                            prediction_file = os.path.basename(filename)
-                            break
+        annotations = labels["annotations"]
+        for annotation in annotations:
+            label = annotation["label"]
+            labels_dict[label] = labels_dict[label] + 1
+        
+        # print(labels_dict)
+        # print("----------------------------------------")
+
+        predictions_file_dict = json.load(open(os.path.join(Predictions_path, game, prediction_file)))
+        
+        predictions = predictions_file_dict["predictions"]
+        for prediction in predictions:
+            confidence = float(prediction["confidence"])
+            if confidence >= 0.8:
+                label = prediction["label"]
+                prediction_dict[label] = prediction_dict[label] + 1
+
+
+        
+        ## Calculate FN 
+        for event in metric_dict.keys():
+            number_of_labels = labels_dict[event]
+            number_of_predicions = prediction_dict[event]
+            
+            ## Calculate TP
+            if number_of_predicions >= number_of_labels:
+                true_positive = number_of_labels
+                false_positive = number_of_predicions - number_of_labels
+                false_negative = 0
             else:
-                for filename in glob.glob(os.path.join(Predictions_path,"*/*/*/*.json")):
-                    prediction_file = os.path.basename(filename)
-                    # print(prediction_file)
-                    break
+                true_positive = number_of_predicions
+                false_positive = 0
+                false_negative = number_of_labels - number_of_predicions
+            
+            metric_dict[event]["True positive"] = metric_dict[event]["True positive"] + true_positive
+            metric_dict[event]["False positive"] = metric_dict[event]["False positive"] + false_positive
+            metric_dict[event]["False negative"] = metric_dict[event]["False negative"] + false_negative
 
-        # Load predictions
-        if zipfile.is_zipfile(Predictions_path):
-            predictions = LoadJsonFromZip(Predictions_path, os.path.join(game, prediction_file))
-        else:
-            predictions = json.load(open(os.path.join(Predictions_path, game, prediction_file)))
-        # convert predictions to vector
-        predictions_half_1, predictions_half_2 = predictions2vector(predictions, num_classes=num_classes, version=version)
+    
+    total_tp = 0
+    total_fp = 0
+    total_fn = 0
 
-        targets_numpy.append(label_half_1)
-        targets_numpy.append(label_half_2)
-        detections_numpy.append(predictions_half_1)
-        detections_numpy.append(predictions_half_2)
-
-        ## Filter on confidence?
-        ## count number of predictions per class
-        ## count number of labels per class
-        ## Calculate TP
-        ## Add these numbers to global 
-
+    for event_name in metric_dict.keys():
+        total_tp += metric_dict[event_name]["True positive"]
+        total_fp += metric_dict[event_name]["False positive"]
+        total_fn += metric_dict[event_name]["False negative"]
+    
+    metric_dict["Total"] = defaultdict(lambda: 0)
+    metric_dict["Total"]["True positive"] = total_tp
+    metric_dict["Total"]["False positive"] = total_fp
+    metric_dict["Total"]["False negative"] = total_fn
+    
+    ## Calculate metrics
+    for event_name in metric_dict.keys():
+        true_positive = metric_dict[event_name]["True positive"]
+        false_positive = metric_dict[event_name]["False positive"]
+        false_negative = metric_dict[event_name]["False negative"]
         
+        if (true_positive + false_positive) != 0:
+            precision = true_positive / (true_positive + false_positive)
+            metric_dict[event_name]["Precision"] = precision
+            recall = true_positive / (true_positive + false_negative)
+            metric_dict[event_name]["Recall"] = recall 
+            metric_dict[event_name]["F1_score"] = 2 * ((precision * recall) / (precision + recall))
 
-        closest_numpy = np.zeros(label_half_1.shape)-1 #10800 x 17 with -1 values.
-        #Get the closest action index
-        for c in np.arange(label_half_1.shape[-1]): #for each class (17 times..)
-            indexes = np.where(label_half_1[:,c] != 0)[0].tolist() #index of Every frame in first half that is annotated with this class.. 
-            if len(indexes) == 0 : #If we have no annotations in first half..
-                continue
-            indexes.insert(0,-indexes[0])
-            indexes.append(2*closest_numpy.shape[0])
-            for i in np.arange(len(indexes)-2)+1: #For all frames annotated with c..
-                start = max(0,(indexes[i-1]+indexes[i])//2)
-                stop = min(closest_numpy.shape[0], (indexes[i]+indexes[i+1])//2)
-                closest_numpy[start:stop,c] = label_half_1[indexes[i],c]
-                exit()
+    pretty_print_metrics(metric_dict)
+
+def pretty_print_metrics(metric_dict):
+    for event_name in metric_dict.keys():
+        print(f"\n{event_name}\n---------------")
+        for key, value in metric_dict[event_name].items():
+            print(f"    {key}: {round(value, 4)}")
         
-        closests_numpy.append(closest_numpy)
-
-        closest_numpy = np.zeros(label_half_2.shape)-1
-        for c in np.arange(label_half_2.shape[-1]):
-            indexes = np.where(label_half_2[:,c] != 0)[0].tolist()
-            if len(indexes) == 0 :
-                continue
-            indexes.insert(0,-indexes[0])
-            indexes.append(2*closest_numpy.shape[0])
-            for i in np.arange(len(indexes)-2)+1:
-                start = max(0,(indexes[i-1]+indexes[i])//2)
-                stop = min(closest_numpy.shape[0], (indexes[i]+indexes[i+1])//2)
-                closest_numpy[start:stop,c] = label_half_2[indexes[i],c]
-        closests_numpy.append(closest_numpy)
