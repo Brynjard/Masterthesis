b4ff93ed75b34aa586c324a34cc6e4da90688bf4
Håkon Hernes <haakhern@g001.cm.cluster>
2022-03-17 14:26:37 +0100

    Changed functions

---
+++ experiments/annotation_evaluation.py
@@ -0,0 +1,501 @@
+import os
+from typing import List
+import numpy as np
+
+from SoccerNet.utils import getListGames
+
+
+import json
+
+from SoccerNet.Evaluation.utils import LoadJsonFromZip, EVENT_DICTIONARY_V2, INVERSE_EVENT_DICTIONARY_V2
+from SoccerNet.Evaluation.utils import EVENT_DICTIONARY_V1, INVERSE_EVENT_DICTIONARY_V1
+
+import json
+import zipfile
+from tqdm import tqdm
+
+# d = None
+# data = None
+# with zipfile.ZipFile("./data/drug-event-Q4-0001-of-0013.json.zip", "r") as z:
+#    for filename in z.namelist():
+#       print(filename)
+#       with z.open(filename) as f:
+#          data = f.read()
+#          d = json.loads(data)
+
+import glob
+
+# from utils import LoadJsonFromZip
+# def LoadJsonFromZip(zippedFile, JsonPath):
+#     with zipfile.ZipFile(zippedFile, "r") as z:
+#         # print(filename)
+#         with z.open(JsonPath) as f:
+#             data = f.read()
+#             d = json.loads(data.decode("utf-8"))
+
+#     return d
+
+
+def evaluate(SoccerNet_path, Predictions_path, delta, prediction_file="results_spotting.json", split="test"):
+    # evaluate the prediction with respect to some ground truth
+    # Params:
+    #   - SoccerNet_path: path for labels (folder or zipped file)
+    #   - Predictions_path: path for predictions (folder or zipped file)
+    #   - prediction_file: name of the predicted files - if set to None, try to infer it
+    #   - split: split to evaluate from ["test", "challenge"]
+    #   - frame_rate: frame rate to evalaute from [2]
+    # Return:
+    #   - details mAP
+    framerate = 2
+    list_games = getListGames(split=split)
+    targets_numpy = list()
+    detections_numpy = list()
+    closests_numpy = list()
+
+    version = 2
+    label_files = "Labels-v2.json"
+    num_classes = 17
+
+    for game in tqdm(list_games):
+
+        # Load labels
+        if zipfile.is_zipfile(SoccerNet_path):
+            labels = LoadJsonFromZip(SoccerNet_path, os.path.join(game, label_files))
+        else:
+            labels = json.load(open(os.path.join(SoccerNet_path, game, label_files)))
+        # convert labels to vector
+        label_half_1, label_half_2 = label2vector(
+            labels, num_classes=num_classes, version=version)
+        # print(version)
+        # print(label_half_1)
+        # print(label_half_2)
+
+
+
+        # infer name of the prediction_file
+        if prediction_file == None:
+            if zipfile.is_zipfile(Predictions_path):
+                with zipfile.ZipFile(Predictions_path, "r") as z:
+                    for filename in z.namelist():
+                        #       print(filename)
+                        if filename.endswith(".json"):
+                            prediction_file = os.path.basename(filename)
+                            break
+            else:
+                for filename in glob.glob(os.path.join(Predictions_path,"*/*/*/*.json")):
+                    prediction_file = os.path.basename(filename)
+                    # print(prediction_file)
+                    break
+
+        # Load predictions
+        if zipfile.is_zipfile(Predictions_path):
+            predictions = LoadJsonFromZip(Predictions_path, os.path.join(game, prediction_file))
+        else:
+            predictions = json.load(open(os.path.join(Predictions_path, game, prediction_file)))
+        # convert predictions to vector
+        predictions_half_1, predictions_half_2 = predictions2vector(predictions, num_classes=num_classes, version=version)
+
+        targets_numpy.append(label_half_1)
+        targets_numpy.append(label_half_2)
+        detections_numpy.append(predictions_half_1)
+        detections_numpy.append(predictions_half_2)
+
+        ## Filter on confidence?
+        ## count number of predictions per class
+        ## count number of labels per class
+        ## Calculate TP
+        ## Add these numbers to global 
+
+
+        closest_numpy = np.zeros(label_half_1.shape)-1 #10800 x 17 with -1 values.
+        #Get the closest action index
+        for c in np.arange(label_half_1.shape[-1]): #for each class (17 times..)
+            indexes = np.where(label_half_1[:,c] != 0)[0].tolist() #index of Every frame in first half that is annotated with this class.. 
+            if len(indexes) == 0 : #If we have no annotations in first half..
+                continue
+            indexes.insert(0,-indexes[0])
+            indexes.append(2*closest_numpy.shape[0])
+            for i in np.arange(len(indexes)-2)+1: #For all frames annotated with c..
+                start = max(0,(indexes[i-1]+indexes[i])//2)
+                stop = min(closest_numpy.shape[0], (indexes[i]+indexes[i+1])//2)
+                closest_numpy[start:stop,c] = label_half_1[indexes[i],c]
+        closests_numpy.append(closest_numpy)
+
+        closest_numpy = np.zeros(label_half_2.shape)-1
+        for c in np.arange(label_half_2.shape[-1]):
+            indexes = np.where(label_half_2[:,c] != 0)[0].tolist()
+            if len(indexes) == 0 :
+                continue
+            indexes.insert(0,-indexes[0])
+            indexes.append(2*closest_numpy.shape[0])
+            for i in np.arange(len(indexes)-2)+1:
+                start = max(0,(indexes[i-1]+indexes[i])//2)
+                stop = min(closest_numpy.shape[0], (indexes[i]+indexes[i+1])//2)
+                closest_numpy[start:stop,c] = label_half_2[indexes[i],c]
+        closests_numpy.append(closest_numpy)
+
+
+
+    return compute_precision_recall_curve(targets_numpy, closests_numpy, detections_numpy, delta)
+
+
+
+
+def label2vector(labels, num_classes=17, framerate=2, version=2):
+
+
+    vector_size = 90*60*framerate
+
+    label_half1 = np.zeros((vector_size, num_classes))
+    label_half2 = np.zeros((vector_size, num_classes))
+
+    for annotation in labels["annotations"]:
+
+        time = annotation["gameTime"]
+        event = annotation["label"]
+
+        half = int(time[0])
+
+        minutes = int(time[-5:-3])
+        seconds = int(time[-2::])
+        frame = framerate * ( seconds + 60 * minutes ) 
+
+        if version == 2:
+            if event not in EVENT_DICTIONARY_V2:
+                continue
+            label = EVENT_DICTIONARY_V2[event]
+        elif version == 1:
+            # print(event)
+            # label = EVENT_DICTIONARY_V1[event]
+            if "card" in event: label = 0
+            elif "subs" in event: label = 1
+            elif "soccer" in event: label = 2
+            else: 
+                # print(event)
+                continue
+        # print(event, label, half)
+
+        value = 1
+        if "visibility" in annotation.keys():
+            if annotation["visibility"] == "not shown":
+                value = -1
+
+        if half == 1:
+            frame = min(frame, vector_size-1)
+            label_half1[frame][label] = value
+
+        if half == 2:
+            frame = min(frame, vector_size-1)
+            label_half2[frame][label] = value
+
+    return label_half1, label_half2
+
+def predictions2vector(predictions, num_classes=17, version=2, framerate=2):
+
+
+    vector_size = 90*60*framerate
+
+    prediction_half1 = np.zeros((vector_size, num_classes))-1
+    prediction_half2 = np.zeros((vector_size, num_classes))-1
+
+    for annotation in predictions["predictions"]:
+
+        time = int(annotation["position"])
+        event = annotation["label"]
+
+        half = int(annotation["half"])
+
+        frame = int(framerate * ( time/1000 ))
+
+        if version == 2:
+            if event not in EVENT_DICTIONARY_V2:
+                continue
+            label = EVENT_DICTIONARY_V2[event]
+        elif version == 1:
+            label = EVENT_DICTIONARY_V1[event]
+            # print(label)
+            # EVENT_DICTIONARY_V1[l]
+            # if "card" in event: label=0
+            # elif "subs" in event: label=1
+            # elif "soccer" in event: label=2
+            # else: continue
+
+        value = annotation["confidence"]
+
+        if half == 1:
+            frame = min(frame, vector_size-1)
+            prediction_half1[frame][label] = value
+
+        if half == 2:
+            frame = min(frame, vector_size-1)
+            prediction_half2[frame][label] = value
+
+    return prediction_half1, prediction_half2
+
+
+import numpy as np
+from tqdm import tqdm
+import time
+np.seterr(divide='ignore', invalid='ignore')
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def NMS(detections, delta):
+    
+    # Array to put the results of the NMS
+    detections_tmp = np.copy(detections)
+    detections_NMS = np.zeros(detections.shape)-1
+
+    # Loop over all classes
+    for i in np.arange(detections.shape[-1]):
+        # Stopping condition
+        while(np.max(detections_tmp[:,i]) >= 0):
+
+            # Get the max remaining index and value
+            max_value = np.max(detections_tmp[:,i])
+            max_index = np.argmax(detections_tmp[:,i])
+
+            detections_NMS[max_index,i] = max_value
+
+            detections_tmp[int(np.maximum(-(delta/2)+max_index,0)): int(np.minimum(max_index+int(delta/2), detections.shape[0])) ,i] = -1
+
+    return detections_NMS
+
+def compute_class_scores(target, closest, detection, delta):
+
+    # Retrieving the important variables
+    gt_indexes = np.where(target != 0)[0]
+    gt_indexes_visible = np.where(target > 0)[0]
+    gt_indexes_unshown = np.where(target < 0)[0]
+    pred_indexes = np.where(detection >= 0)[0]
+    pred_scores = detection[pred_indexes]
+
+    # Array to save the results, each is [pred_scor,{1 or 0}]
+    game_detections = np.zeros((len(pred_indexes),3))
+    game_detections[:,0] = np.copy(pred_scores)
+    game_detections[:,2] = np.copy(closest[pred_indexes])
+
+
+    remove_indexes = list()
+
+    for gt_index in gt_indexes:
+
+        max_score = -1
+        max_index = None
+        game_index = 0
+        selected_game_index = 0
+
+        for pred_index, pred_score in zip(pred_indexes, pred_scores):
+
+            if pred_index < gt_index - delta:
+                game_index += 1
+                continue
+            if pred_index > gt_index + delta:
+                break
+
+            if abs(pred_index-gt_index) <= delta/2 and pred_score > max_score and pred_index not in remove_indexes:
+                max_score = pred_score
+                max_index = pred_index
+                selected_game_index = game_index
+            game_index += 1
+
+        if max_index is not None:
+            game_detections[selected_game_index,1]=1
+            remove_indexes.append(max_index)
+
+    return game_detections, len(gt_indexes_visible), len(gt_indexes_unshown)
+
+
+
+def compute_precision_recall_curve(targets, closests, detections, delta):
+    
+    # Store the number of classes
+    num_classes = targets[0].shape[-1]
+
+    # 200 confidence thresholds between [0,1]
+    thresholds = np.linspace(0,1,200) #array of evenly spaced values between 0 - 1, (200 elements)
+
+    # Store the precision and recall points
+    precision = list()
+    recall = list()
+    precision_visible = list()
+    recall_visible = list()
+    precision_unshown = list()
+    recall_unshown = list()
+    true_positive = list()
+    true_positive_visible = list()
+    true_positive_unshown = list()
+    false_positive = list()
+    false_positive_visible = list()
+    false_positive_unshown = list()
+    false_negative = list()
+    false_negative_visible = list()
+    false_negative_unshown = list()
+
+
+    # Apply Non-Maxima Suppression if required
+    start = time.time()
+
+    # Precompute the predictions scores and their correspondence {TP, FP} for each class
+    for c in np.arange(num_classes):
+        total_detections =  np.zeros((1, 3)) #[[0. 0. 0.]]
+        total_detections[0,0] = -1
+        n_gt_labels_visible = 0
+        n_gt_labels_unshown = 0
+        
+        # Get the confidence scores and their corresponding TP or FP characteristics for each game
+        for target, closest, detection in zip(targets, closests, detections):
+            tmp_detections, tmp_n_gt_labels_visible, tmp_n_gt_labels_unshown = compute_class_scores(target[:,c], closest[:,c], detection[:,c], delta)
+            total_detections = np.append(total_detections,tmp_detections,axis=0)
+            n_gt_labels_visible = n_gt_labels_visible + tmp_n_gt_labels_visible
+            n_gt_labels_unshown = n_gt_labels_unshown + tmp_n_gt_labels_unshown
+
+        precision.append(list())
+        recall.append(list())
+        precision_visible.append(list())
+        recall_visible.append(list())
+        precision_unshown.append(list())
+        recall_unshown.append(list())
+        ###
+        true_positive.append(list())
+        true_positive_visible.append(list())
+        true_positive_unshown.append(list())
+        false_positive.append(list())
+        false_positive_visible.append(list())
+        false_positive_unshown.append(list())
+        false_negative.append(list())
+        false_negative_visible.append(list())
+        false_negative_unshown.append(list())
+
+        # Get only the visible or unshown actions
+        total_detections_visible = np.copy(total_detections)
+        total_detections_unshown = np.copy(total_detections)
+        total_detections_visible[np.where(total_detections_visible[:,2] <= 0.5)[0],0] = -1
+        total_detections_unshown[np.where(total_detections_unshown[:,2] >= -0.5)[0],0] = -1
+
+        # Get the precision and recall for each confidence threshold
+        for threshold in thresholds:
+            pred_indexes = np.where(total_detections[:,0]>=threshold)[0]
+            pred_indexes_visible = np.where(total_detections_visible[:,0]>=threshold)[0]
+            pred_indexes_unshown = np.where(total_detections_unshown[:,0]>=threshold)[0]
+            
+            TP = np.sum(total_detections[pred_indexes,1])
+            TP_visible = np.sum(total_detections[pred_indexes_visible,1])
+            TP_unshown = np.sum(total_detections[pred_indexes_unshown,1])
+
+            FP = len(pred_indexes) - TP
+            FP_visible = len(pred_indexes_visible) - TP_visible
+            FP_unshown = len(pred_indexes_unshown) - TP_unshown
+
+            FN = (n_gt_labels_visible + n_gt_labels_unshown) - TP
+            FN_visible = n_gt_labels_visible - TP_visible
+            FN_unshown = n_gt_labels_unshown - TP_unshown
+
+            p = np.nan_to_num(TP/len(pred_indexes))
+            r = np.nan_to_num(TP/(n_gt_labels_visible + n_gt_labels_unshown))
+            p_visible = np.nan_to_num(TP_visible/len(pred_indexes_visible))
+            r_visible = np.nan_to_num(TP_visible/n_gt_labels_visible)
+            p_unshown = np.nan_to_num(TP_unshown/len(pred_indexes_unshown))
+            r_unshown = np.nan_to_num(TP_unshown/n_gt_labels_unshown)
+
+            precision[-1].append(p)
+            recall[-1].append(r)
+            precision_visible[-1].append(p_visible)
+            recall_visible[-1].append(r_visible)
+            precision_unshown[-1].append(p_unshown)
+            recall_unshown[-1].append(r_unshown)
+
+            true_positive[-1].append(TP)
+            true_positive_visible[-1].append(TP_visible)
+            true_positive_unshown[-1].append(TP_unshown)
+            false_positive[-1].append(FP)
+            false_positive_visible[-1].append(FP_visible)
+            false_positive_unshown[-1].append(FP_unshown)
+            false_negative[-1].append(FN)
+            false_negative_visible[-1].append(FN_visible)
+            false_negative_unshown[-1].append(FN_unshown)
+
+
+    precision = np.array(precision).transpose()
+    recall = np.array(recall).transpose()
+    precision_visible = np.array(precision_visible).transpose()
+    recall_visible = np.array(recall_visible).transpose()
+    precision_unshown = np.array(precision_unshown).transpose()
+    recall_unshown = np.array(recall_unshown).transpose()
+
+    true_positive = np.array(true_positive).transpose()
+    true_positive_visible = np.array(true_positive_visible).transpose()
+    true_positive_unshown = np.array(true_positive_unshown).transpose()
+    false_positive = np.array(false_positive).transpose()
+    false_positive_visible = np.array(false_positive_visible).transpose()
+    false_positive_unshown = np.array(false_positive_unshown).transpose()
+    false_negative = np.array(false_negative).transpose()
+    false_negative_visible = np.array(false_negative_visible).transpose()
+    false_negative_unshown = np.array(false_negative_unshown).transpose()
+
+
+
+    # # Sort the points based on the recall, class per class
+    # for i in np.arange(num_classes):
+    #     index_sort = np.argsort(recall[:,i])
+    #     precision[:,i] = precision[index_sort,i]
+    #     recall[:,i] = recall[index_sort,i]
+    #     true_positive[:,i] = true_positive[index_sort, i]
+    #     false_positive[:,i] = false_positive[index_sort, i]
+    #     false_negative[:,i] = false_negative[index_sort, i]
+
+    # # Sort the points based on the recall, class per class
+    # for i in np.arange(num_classes):
+    #     index_sort = np.argsort(recall_visible[:,i])
+    #     precision_visible[:,i] = precision_visible[index_sort,i]
+    #     recall_visible[:,i] = recall_visible[index_sort,i]
+    #     true_positive_visible[:,i] = true_positive_visible[index_sort, i]
+    #     false_positive_visible[:,i] = false_positive_visible[index_sort, i]
+    #     false_negative_visible[:,i] = false_negative_visible[index_sort, i]
+
+    # # Sort the points based on the recall, class per class
+    # for i in np.arange(num_classes):
+    #     index_sort = np.argsort(recall_unshown[:,i])
+    #     precision_unshown[:,i] = precision_unshown[index_sort,i]
+    #     recall_unshown[:,i] = recall_unshown[index_sort,i]
+    #     true_positive_unshown[:,i] = true_positive_unshown[index_sort, i]
+    #     false_positive_unshown[:,i] = false_positive_unshown[index_sort, i]
+    #     false_negative_unshown[:,i] = false_negative_unshown[index_sort, i]
+
+    return precision, recall, precision_visible, recall_visible, precision_unshown, recall_unshown, \
+            true_positive, true_positive_visible, true_positive_unshown, \
+            false_positive, false_positive_visible, false_positive_unshown, \
+            false_negative, false_negative_visible, false_negative_unshown
+
+def get_value_from_confidence_level(array, confidence_threshold):
+    x = confidence_threshold / 0.005
+    
+
+
+if __name__ == '__main__':
+    delta = 30
+    precision, recall, precision_visible, recall_visible, precision_unshown, recall_unshown, \
+    true_positive, true_positive_visible, true_positive_unshown, \
+    false_positive, false_positive_visible, false_positive_unshown, \
+    false_negative, false_negative_visible, false_negative_unshown = evaluate("/global/D1/projects/soccer_clipping/haakhern_brynjabm_thesis/original_features", "/global/D1/projects/soccer_clipping/haakhern_brynjabm_thesis/haakon/ContextAwareActionSpotting/experiments/experiment_5/fusion_out", delta=delta, prediction_file="results_spotting.json", split="test",)
+    # print((np.arange(12)*5 + 5)*2)
+
+    print(true_positive[0].shape)
+    
+    
