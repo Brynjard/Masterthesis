369fa16b54c7dbd3e7e6349d5c4a3b5216079732
Brynjard Buvarp Misvær <brynjabm@g001.cm.cluster>
2022-03-25 11:18:16 +0100

    table for classwise stats, ++

--- experiments/barplots_lgc.py
+++ experiments/barplots_lgc.py
@@ -1,3 +1,4 @@
+from multiprocessing import get_start_method
 import numpy as np
 from event_list import EVENT_LIST 
 from collections import defaultdict
@@ -32,8 +33,6 @@ def create_highest_scores(data): #returns a array: #9 x 17: Rows: Metrics (f1, f
         m = data[i]
         for c in range(17):
             highest_threshold_i = np.argsort(m[:,c])[-1]
-            highest_threshold = round(highest_threshold_i * 0.005, 2)
-            highest_threshold_score = m[highest_threshold_i, c]
 
             index_overview[c][j] = highest_threshold_i
 
@@ -49,7 +48,6 @@ def create_highest_scores(data): #returns a array: #9 x 17: Rows: Metrics (f1, f
                 scores[i][c] = curr_data[int(index_overview[c][2])][c]
     return scores
 
-    
 def create_barplot(baseline_scores, ex_scores, metric):
     filename = "barplot_comparison_lgc_60_{}.png".format(metric)
     X = EVENT_LIST
@@ -109,7 +107,6 @@ def create_table_for_barplot(baseline_scores, ex_scores, metric):
 
     table = template_table_start + data_string + template_table_end
 
-    print("-----------------TABLE FOR: {} -----------------".format(metric))
     table_string = "-----------------TABLE FOR: {} -----------------\n\n".format(metric) + table
     return table_string
 
@@ -165,6 +162,15 @@ def get_summary_scores(model_data, model_name):
                 best_i = int(index_overview[c][2])
                 total_FN_unshown += curr_data[best_i][c]
 
+    precision_total = round(total_TP / (total_TP + total_FP), 2)
+    precision_visible = round(total_TP_visible / (total_TP_visible + total_FP_visible), 2)
+    precision_unshown = round(total_TP_unshown / (total_TP_unshown + total_FP_unshown), 2)
+    recall_total = round(total_TP / (total_TP + total_FN), 2)
+    recall_visible =  round(total_TP_visible / (total_TP_visible + total_FN_visible), 2)
+    recall_unshown = round(total_TP_unshown / (total_TP_unshown + total_FN_unshown), 2)
+    f1_total = round(2 * ((precision_total * recall_total) / (precision_total + recall_total)), 2)
+    f1_visible = round(2 * ((precision_visible * recall_visible) / (precision_visible + recall_visible)), 2)
+    f1_unshown = round(2 * ((precision_unshown * recall_unshown) / (precision_unshown + recall_unshown)), 2)
     print("--------------- TABLE FOR TOTAL SCORES ({})----------------".format(model_name))
     print("TP: {}".format(total_TP))
     print("TP(Visible) {}".format(total_TP_visible))
@@ -175,38 +181,182 @@ def get_summary_scores(model_data, model_name):
     print("FN: {}".format(total_FN))
     print("FN(Visible) {}".format(total_FN_visible))
     print("FN(unshown): {}".format(total_FN_unshown))
-    precision_total = round(total_TP / (total_TP + total_FP), 2)
     print("PRECISION: {}".format(precision_total))
-    precision_visible = round(total_TP_visible / (total_TP_visible + total_FP_visible), 2)
     print("PRECISION (Visible): {}".format(precision_visible))
-    precision_unshown = round(total_TP_unshown / (total_TP_unshown + total_FP_unshown), 2)
     print("PRECISION (unshown): {}".format(precision_unshown))
-    recall_total = round(total_TP / (total_TP + total_FN), 2)
     print("RECALL: {}".format(recall_total))
-    recall_visible =  round(total_TP_visible / (total_TP_visible + total_FN_visible), 2)
     print("RECALL(VISIBLE): {}".format(recall_visible))
-    recall_unshown = round(total_TP_unshown / (total_TP_unshown + total_FN_unshown), 2)
     print("RECALL (unshown): {}".format(recall_unshown))
-    f1_total = round(2 * ((precision_total * recall_total) / (precision_total + recall_total)), 2)
     print("F1: {}".format(f1_total))
-    f1_visible = round(2 * ((precision_visible * recall_visible) / (precision_visible + recall_visible)), 2)
     print("F1(visible): {}".format(f1_visible))
-    f1_unshown = round(2 * ((precision_unshown * recall_unshown) / (precision_unshown + recall_unshown)), 2)
     print("F1(unshown): {}".format(f1_unshown))
+
+def get_table_optimal_confidence_thresholds_for_model(baseline_data, ex_data):
+    template_table_start = """
+    \\begin{table}[h!bt]
+    \\centering
+    \\footnotesize
+    \\makebox[\\textwidth][c]{
+    \\begin{tabularx}{552pt}{|c|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|}
+    \\toprule
+    Threshold & \\rotatebox{90}{Ball out of play} & \\rotatebox{90}{Throw-in} & \\rotatebox{90}{Foul} & \\rotatebox{90}{Ind. free-kick} & \\rotatebox{90}{Clearance} & \\rotatebox{90}{Shots on tar.} & \\rotatebox{90}{Shots off tar.} & \\rotatebox{90}{Corner} & \\rotatebox{90}{Substitution} & \\rotatebox{90}{Kick-off} & \\rotatebox{90}{Yellow card} & \\rotatebox{90}{Offside} & \\rotatebox{90}{Dir. free-kick} & \\rotatebox{90}{Goal} & \\rotatebox{90}{Penalty} & \\rotatebox{90}{Yel. to Red} & \\rotatebox{90}{Red card} \\\\
+    \\midrule
+    """
+    template_table_end = """
+    \\bottomrule
+    \\end{tabularx}
+    }
+    \\caption{CAPTION HERE}
+    \\label{table:f1_score_per_class_baseline}
+    \\end{table}
+    """
+    #BASELINE:
+    index_overview_baseline = np.zeros((17, 3)) #rows: classes, cols: f1_index, f1_visible_index, f1_unshown_index
+    for i in range(6, 9):
+        j = i - 6
+        m = baseline_data[i]
+        for c in range(17):
+            highest_threshold_i = np.argsort(m[:,c])[-1]
+            index_overview_baseline[c][j] = highest_threshold_i
+    #OUR MODEL:
+    index_overview_ex = np.zeros((17, 3)) #rows: classes, cols: f1_index, f1_visible_index, f1_unshown_index
+    for i in range(6, 9):
+        j = i - 6
+        m = ex_data[i]
+        for c in range(17):
+            highest_threshold_i = np.argsort(m[:,c])[-1]
+            index_overview_ex[c][j] = highest_threshold_i
+
+    table_string = """"""
+    threshold_category = ["all", "visible", "off-screen"]
+    for s in range(0, 3):
+        table_string += "{} & & & & & & & & & & & & & & & & &\\\\\n".format(threshold_category[s])
+        thresholds_baseline = index_overview_baseline[:,s]
+        thresholds_ex = index_overview_ex[:, s]
+        table_string += "Baseline & ".format(threshold_category[s])
+        for i, t_b in enumerate(thresholds_baseline):
+            if i == len(thresholds_baseline) - 1:
+                table_string += "{}\\\\\n".format(round(t_b * 0.005, 2))
+                table_string += "Our model & ".format(threshold_category[s])
+                for i, t_ex in enumerate(thresholds_ex):
+                    if i == len(thresholds_ex) - 1:
+                        table_string += "{}\\\\\n".format(round(t_ex * 0.005, 2))
+                    else:
+                        table_string += "{} & ".format(round(t_ex * 0.005, 2))
+            else:
+                table_string += "{} & ".format(round(t_b * 0.005, 2))
+    print("TABLE OF OPTIMAL CONFIDENCE THRESHOLDS FOR BASELINE/OUR MODEL:")
+    print(template_table_start + table_string + template_table_end)
+
+
+def get_stats_classwise(model_data):
+    #Getting for F1(all):
+    index_overview = np.zeros((17, 3)) #rows: classes, cols: f1_index, f1_visible_index, f1_unshown_index
+    for i in range(6, 9):
+        j = i - 6
+        m = model_data[i]
+        for c in range(17):
+            highest_threshold_i = np.argsort(m[:,c])[-1]
+
+            index_overview[c][j] = highest_threshold_i
     
+    for m in range(9, 18):
+        if m in [9, 12, 15]:
+            confidence_i = int(index_overview[c][0])
+        elif m in [10, 13, 16]:
+            confidence_i = int(index_overview[c][1])
+        else:
+            confidence_i = int(index_overview[c][2])
+        for c in range(0, 17):
+            print("{} for class: {}: {}".format(list(metrics.keys())[m], EVENT_LIST[c], model_data[m][confidence_i][c]))
+
+def create_classwise_stats_table(model_data, model_name):
+    template_table_start = """
+    \\begin{table}[h!bt]
+    \\centering
+    \\footnotesize
+    \\makebox[\\textwidth][c]{
+    \\begin{tabularx}{520pt}{|c|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|c@{\\hspace{3pt}}|}
+    \\toprule
+    Metric & \\rotatebox{90}{Ball out of play} & \\rotatebox{90}{Throw-in} & \\rotatebox{90}{Foul} & \\rotatebox{90}{Ind. free-kick} & \\rotatebox{90}{Clearance} & \\rotatebox{90}{Shots on tar.} & \\rotatebox{90}{Shots off tar.} & \\rotatebox{90}{Corner} & \\rotatebox{90}{Substitution} & \\rotatebox{90}{Kick-off} & \\rotatebox{90}{Yellow card} & \\rotatebox{90}{Offside} & \\rotatebox{90}{Dir. free-kick} & \\rotatebox{90}{Goal} & \\rotatebox{90}{Penalty} & \\rotatebox{90}{Yel. to Red} & \\rotatebox{90}{Red card} \\\\
+    \\midrule
+    """
+    template_table_end = """
+    \\bottomrule
+    \\end{tabularx}
+    }
+    \\caption{CAPTION HERE}
+    \\label{table:f1_score_per_class_baseline}
+    \\end{table}
+    """
+
+    index_overview = np.zeros((17, 3)) #rows: classes, cols: f1_index, f1_visible_index, f1_unshown_index
+    for i in range(6, 9):
+        j = i - 6
+        m = model_data[i]
+        for c in range(17):
+            highest_threshold_i = np.argsort(m[:,c])[-1]
+            index_overview[c][j] = highest_threshold_i
+    visibility = ""
+    metric = ""
+    table_string = """"""
+    for m in range(9, 18):
+        if m in [9, 12, 15]:
+            confidence_i = int(index_overview[c][0])
+            metric = "TP"
+        elif m in [10, 13, 16]:
+            confidence_i = int(index_overview[c][1])
+            metric = "FP"
+        else:
+            confidence_i = int(index_overview[c][2])
+            metric = "FN"
+        if m == 9:
+            visibility = "All"
+        elif m == 12:
+            visibility = "Visible"
+        else:
+            visibility = "Off-screen"
+        if m in [9, 12, 15]:
+            table_string += "{} & & & & & & & & & & & & & & & & &\\\\\n".format(visibility)
+        table_string += "{} & ".format(metric)
+        for c in range(0, 17):
+            if m in [9, 12, 15]:
+                confidence_i = int(index_overview[c][0])
+            elif m in [10, 13, 16]:
+                confidence_i = int(index_overview[c][1])
+            else:
+                confidence_i = int(index_overview[c][2])
+        
+            if c < 16:
+                table_string += "{} & ".format(int(model_data[m][confidence_i][c]))
+            else:
+                table_string += "{} \\\\".format(int(model_data[m][confidence_i][c]))
+            #print("{} for class: {}: {}".format(list(metrics.keys())[m], EVENT_LIST[c], model_data[m][confidence_i][c]))
+    table = template_table_start + table_string + template_table_end
+    print("============STATS-TABLE FOR: {}============".format(model_name))
+    print(table)
 
 
-data = np.load("/global/D1/projects/soccer_clipping/haakhern_brynjabm_thesis/brynjard/ContextAwareActionSpotting/experiments/use_case_live_game_commentary/metrics_baseline_delta_60.npy")
+data_our_model = np.load("/global/D1/projects/soccer_clipping/haakhern_brynjabm_thesis/brynjard/ContextAwareActionSpotting/experiments/use_case_live_game_commentary/metrics_our_model_delta_60.npy")
+data_baseline = np.load("/global/D1/projects/soccer_clipping/haakhern_brynjabm_thesis/brynjard/ContextAwareActionSpotting/experiments/use_case_live_game_commentary/metrics_baseline_delta_60.npy")
+print(data_baseline.shape)
+"""
 baseline_scores = create_highest_scores(data)
-#get_summary_scores(data, "baseline")
+get_summary_scores(data, "baseline")
 
-data = np.load("/global/D1/projects/soccer_clipping/haakhern_brynjabm_thesis/brynjard/ContextAwareActionSpotting/experiments/use_case_live_game_commentary/metrics_our_model_delta_60.npy")
 ex_scores = create_highest_scores(data)
-#get_summary_scores(data, "Our model")
+get_summary_scores(data, "Our model")"""
+#get_table_optimal_confidence_thresholds_for_model(data_baseline, data_our_model)
+#print("STATS FOR BASELINE:")
+#get_stats_classwise(data_baseline)
+#print("STATS FOR OUR MODEL: ")
+#get_stats_classwise(data_our_model)
+#for m in range(0, 9):  
+    #create_barplot(baseline_scores, ex_scores, list(metrics.keys())[m])
+    #print(create_table_for_barplot(baseline_scores, ex_scores, list(metrics.keys())[m]))
 
-for m in range(0, 9):  
-    create_barplot(baseline_scores, ex_scores, list(metrics.keys())[m])
-    print(create_table_for_barplot(baseline_scores, ex_scores, list(metrics.keys())[m]))
+create_classwise_stats_table(data_baseline, "baseline")
+create_classwise_stats_table(data_our_model, "our Model")
     
 
 
